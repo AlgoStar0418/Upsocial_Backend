ec54a1bfb4b16f0b37387827276393332af486e5
AlgoStar0418 <bs07120418@gmail.com>
2023-04-22 23:33:56 +0200

    update backend

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
     res.setHeader("Access-Control-Allow-Credentials", true);
-
     next();
 });
 
