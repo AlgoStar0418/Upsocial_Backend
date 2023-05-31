a01769c74efb9215677953be13e5e5a4c0284197
AlgoStar0418 <bs07120418@gmail.com>
2023-06-01 04:31:27 +0900

    update|add.upsocial

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 require("dotenv").config();
 var express = require("express");
-var app = express();
+const cors = require('cors');
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+var app = express();
 const port = process.env.PORT;
 const Route = require("./routes/inxex");// Import Routers.
 const downloadRoute = require('./routes/download.router');
-const cors = require('cors');
+const logger = require('morgan');
 
 
 app.use(cors());
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ limit: '800mb' }));
-app.use(express.static("downloads"));
-
-// Use routes
-// app.use("/api/Upsocial", Route);
-
-// add.upsocial
-app.use('/api', downloadRoute);
-
 // enable CORS without external module
 app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -29,5 +20,31 @@ app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 });
+
+// Body parser middleware
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: '800mb' }));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("downloads"));
+
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        expires: 600000
+    }
+}));
+
+// Use routes
+// app.use("/api/Upsocial", Route);
+
+// add.upsocial
+app.use('/api', downloadRoute);
+
+
 
 app.listen(port, () => console.log(`Backend API is now running on port: ${port}`));
