63b9910ac205957a1e922790b27e689fcf2253be
AlgoStar0418 <bs07120418@gmail.com>
2023-06-06 22:19:22 +0900

    update

--- controller/Controller.js
+++ controller/Controller.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const OrbitDB = require("orbit-db");
 const axios = require('axios');
-const IPFS = require("ipfs-http-client");
+const { create } = require("ipfs-http-client");
 const { exec } = require('node:child_process');
 const fs = require('fs');
 const filesize = require("file-size");
@@ -51,7 +51,7 @@ const items = [
     { id: 24, name: 'Blogs' },
 ];
 
-let ipfs;
+let ipfs = create(new URL('http://localhost:5001'));
 let orbitdb;
 
 
@@ -68,12 +68,12 @@ let ENCRYPT_PASS = process.env.ENCRYPT_PASS;
 
 exports.CreateDBs = async (req, res) => {
     if (userDataDB == undefined && contentDB == undefined && channelDB == undefined && playlistDB == undefined) {
-        ipfs = await IPFS.create({
-            EXPERIMENTAL: {
-                pubsub: true,
-            },
-            repo: "UpsocialRepo"
-        });
+        // ipfs = await IPFS.create({
+        //     EXPERIMENTAL: {
+        //         pubsub: true,
+        //     },
+        //     repo: "UpsocialRepo"
+        // });
 
         orbitdb = await OrbitDB.createInstance(ipfs, {});
 
